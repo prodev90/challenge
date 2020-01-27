6b56ba0afe444e02e314050bfda67f997895c537
prodev1991715 <YR199111RY@protonmail.com>
2020-01-28 05:30:08 +0900

    feat: Add user-agent endpoint

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+<<<<<<< HEAD
   if (req.url === '/base64') return respondBase64(req, res)
+=======
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
+>>>>>>> feat: Add user-agent endpoint
 
   res.end()
 })
@@ -13,8 +17,13 @@ function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
 
+<<<<<<< HEAD
 function respondBase64 (req, res) {
   res.end(JSON.stringify({ msg: 'Thank you for your test.' }))
+=======
+function respondUserAgent (req, res) {
+  res.end(JSON.stringify({ msg: 'I am very interested this test.' }))
+>>>>>>> feat: Add user-agent endpoint
 }
 
 server.listen(PORT)
